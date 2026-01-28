8ae4b19bdb79981dfbe950de306bc0f1b197b383
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2026-01-27 19:39:53 -0600

    Practica 1 Completa

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI  # <-- Corregido: FastAPI, no FASTAPI
-
-app = FastAPI()  # <-- Corregido: FastAPI()
+from fastapi import FastAPI  
+import asyncio
+#Instancia del servidor
+app = FastAPI()
 
 @app.get("/")  # Endpoint de inicio, todos los endpoints se acompañan de una función
 async def bienvenida():
     return {"mensaje": "¡Bienvenido a mi API!"}  # Formato JSON
+
+@app.get("/HolaMundo")  # Endpoint
+async def hola():
+    await asyncio.sleep(7)#Simulacion de uns 
+    return {"mensaje": "¡Hola Mundo FastAPI!",
+            "estatus":"200"
+            }  # Formato JSON

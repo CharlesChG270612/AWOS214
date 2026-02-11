40036817c34e41bb17a04fcba3b178d4247a8d4f
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2026-02-10 22:01:51 -0600

    Practica 2_Documentacion automatica COMPLETADA

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI  
 import asyncio
+from typing import Optional
 #Instancia del servidor
-app = FastAPI()
+app = FastAPI(
+    title="Mi primer API",
+    description="Carlos",
+    version="1.0.0"
+    )
 
-@app.get("/")  # Endpoint de inicio, todos los endpoints se acompañan de una función
+usuarios=[
+    {"id":1,"nombre":"Juan","edad":21},
+    {"id":2,"nombre":"Israel","edad":21},
+    {"id":3,"nombre":"Sofi","edad":21}
+]
+
+@app.get("/",tags=["Inicio"])  # Endpoint de inicio, todos los endpoints se acompañan de una función
 async def bienvenida():
     return {"mensaje": "¡Bienvenido a mi API!"}  # Formato JSON
 
-@app.get("/HolaMundo")  # Endpoint
+@app.get("/HolaMundo",tags=["Bienvenida asincrona"])  # Endpoint
 async def hola():
     await asyncio.sleep(7)#Simulacion de uns 
     return {"mensaje": "¡Hola Mundo FastAPI!",
             "estatus":"200"
             }  # Formato JSON
+
+@app.get("/v1/usuario/{id}",tags=["Parametro Obligatorio"])  # Endpoint de inicio, todos los endpoints se acompañan de una función
+async def consultaUno(id:int):
+    return {"Se encontró el usuario": id}  # Formato JSON
+
+@app.get("/v1/usuario/",tags=["Parametro Opcional"])  # Endpoint de inicio, todos los endpoints se acompañan de una función
+async def consultaTodos(id:Optional[int]=None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"] == id:
+                return{"Mensaje": "Usuario encontrado", "Usuario": usuario}
+        return{"Mensaje": "Usuario no encontrado", "Usuario": id}
+    else:
+        return{"Mensaje": "No se proporcionó ID"}
+
